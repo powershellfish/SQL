d4131fcf2fba7a6f540f8e9983dd34c0d0accea8
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-12-01 12:45:50 -0700

    Update new_york_restaurants.sql
    Added my solutions to the NYR project.

--- new_york_restaurants.sql
+++ new_york_restaurants.sql
@@ -1,17 +1,69 @@
 # New York Restaurants Project
 
-# Pulls all distinct neighborhoods from the neighborhood column
+# Returns all distinct neighborhoods from the neighborhood column
 
 SELECT DISTINCT neighborhood
 FROM nomnom;
 
-# Pulls all distinct cuisine from the cuisine column
+# Returns all distinct cuisine from the cuisine column
 
 SELECT DISTINCT cuisine
 FROM nomnom;
 
-# Selects all columns from nomnom table but only where the cuisine is Chinese
+# Returns all columns from nomnom table but only where the cuisine is Chinese
 
 SELECT *
 FROM nomnom
 WHERE cuisine = 'Chinese'
+
+# Returns all restaurants with a review of 4 or higher
+
+SELECT *
+FROM nomnom
+WHERE review >= 4;
+
+# Returns all restaurants that are Italian cuisine and $$$
+
+SELECT *
+FROM nomnom
+WHERE cuisine = 'Italian'
+    AND price = '$$$'
+
+# Returns all results that contain 'meatball' in the restaurant name
+
+SELECT *
+FROM nomnom
+WHERE name LIKE '%meatball%';
+
+# Returns all results that are located in Midtown, Downtown, or Chinatown
+
+SELECT *
+FROM nomnom
+WHERE neighborhood = 'Midtown'
+  OR neighborhood = 'Downtown'
+  OR neighborhood = 'Chinatown';
+
+# Returns all results where there isn't a health score for a restaurant
+
+SELECT *
+FROM nomnom
+WHERE health IS NULL;
+
+# Returns top 10  reviewed restaurants in descending order 
+
+SELECT *
+FROM nomnom
+ORDER BY review DESC
+LIMIT 10;
+
+# Changes the rating system from numbers to words and renames the column to 'Review'
+
+SELECT review,
+  CASE
+    WHEN review > 4.5 THEN 'Extraordinary'
+    WHEN review > 4 THEN 'Excellent'
+    WHEN review > 3 THEN 'Good'
+    WHEN review > 2 THEN 'Fair'
+    ELSE 'Poor'
+  END AS 'Review'
+FROM nomnom;
