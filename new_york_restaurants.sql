e1f9cd7312635404902821448183405e2f688205
Ginger <47839113+powershellfish@users.noreply.github.com>
2020-12-01 12:15:02 -0700

    Update new_york_restaurants.sql
    Added comments to explain queries.

--- new_york_restaurants.sql
+++ new_york_restaurants.sql
@@ -1,10 +1,16 @@
 # New York Restaurants Project
 
+# Pulls all distinct neighborhoods from the neighborhood column
+
 SELECT DISTINCT neighborhood
 FROM nomnom;
 
+# Pulls all distinct cuisine from the cuisine column
+
 SELECT DISTINCT cuisine
 FROM nomnom;
+
+# Selects all columns from nomnom table but only where the cuisine is Chinese
 
 SELECT *
 FROM nomnom
